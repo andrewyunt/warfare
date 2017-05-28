cd77fb58ff6d2101e252e0dfef66842ac3ee8269
Andrew Yunt <andrew.yunt@gmail.com>
2017-05-27 17:48:32 -0700

    Remove empty check for available cages

--- src/com/andrewyunt/warfare/listeners/PlayerGameListener.java
+++ src/com/andrewyunt/warfare/listeners/PlayerGameListener.java
@@ -49,7 +49,7 @@ public class PlayerGameListener extends PlayerListener {
         if (Warfare.getInstance().getArena().isEdit()) {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, ChatColor.RED + "The map is currently in edit mode.");
         }
-        else if((game.getStage() == Game.Stage.WAITING || game.getStage() == Game.Stage.COUNTDOWN) && game.getAvailableCages().size() <= 0 && !game.getCages().isEmpty()){
+        else if((game.getStage() == Game.Stage.WAITING || game.getStage() == Game.Stage.COUNTDOWN) && game.getAvailableCages().size() <= 0){
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, ChatColor.RED + "Server is currently full");
         }
         else if(game.getStage() == Game.Stage.RESTART){
