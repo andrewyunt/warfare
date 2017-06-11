3b9060c0e3441d809bdd39b1e984efbf39375ecf
Jacob Evans <levansj01@gmail.com>
2017-06-11 23:52:04 +0100

    Full server restart is not needed

--- src/com/andrewyunt/warfare/game/Game.java
+++ src/com/andrewyunt/warfare/game/Game.java
@@ -5,9 +5,11 @@ import com.andrewyunt.warfare.game.events.AddPlayerEvent;
 import com.andrewyunt.warfare.game.events.RemovePlayerEvent;
 import com.andrewyunt.warfare.game.events.StageChangeEvent;
 import com.andrewyunt.warfare.game.loot.LootChest;
+import com.andrewyunt.warfare.managers.mongo.MongoStorageManager;
 import com.andrewyunt.warfare.player.GamePlayer;
 import lombok.Getter;
 import lombok.Setter;
+import org.bson.types.ObjectId;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -16,6 +18,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -61,14 +65,11 @@ public class Game {
 
 	@Getter private final Set<GamePlayer> players = new HashSet<>();
 	@Getter private final Set<Side> sides = new HashSet<>();
+	@Getter @Setter private int games = 0;
+	@Getter private final int maxGames = 10 + ThreadLocalRandom.current().nextInt(5);
+	@Getter private final long startupTime = System.currentTimeMillis();
 	
 	public Game() {
-		BukkitScheduler scheduler = Warfare.getInstance().getServer().getScheduler();
-		scheduler.scheduleSyncRepeatingTask(Warfare.getInstance(), () -> {
-			if (mapLocation != null) {
-				mapLocation.getWorld().setTime(6000);
-			}
-		}, 20L, 0L);
 	}
 
 	public void setTeams(boolean teams) {
@@ -175,4 +176,24 @@ public class Game {
 			Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + ChatColor.BOLD.toString() + "All chests have been refilled");
 		}
 	}
+
+    public boolean needsRestart(){
+	    return games >= maxGames || System.currentTimeMillis() - startupTime > TimeUnit.DAYS.toMillis(7);
+    }
+
+	public void resetGame(){
+        MongoStorageManager mongoStorageManager = (MongoStorageManager) Warfare.getInstance().getStorageManager();
+        mongoStorageManager.setHasInserted(false);
+        mongoStorageManager.setServerId(new ObjectId());
+        games++;
+        for(Cage cage: cages){
+            cage.setPlayer(null);
+            cage.setBlocks();
+        }
+        players.clear();
+        countdownTime = 0;
+        gameTime = 0;
+        edit = false;
+        setStage(Stage.WAITING);
+    }
 }
