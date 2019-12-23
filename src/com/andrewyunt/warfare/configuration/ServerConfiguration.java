74b360856f75f7018647693bf4df4b9531a2cfcb
EllissaPeterson <ellissa@iastate.edu>
2019-12-23 03:34:03 -0600

    removing wildcards

--- src/com/andrewyunt/warfare/configuration/ServerConfiguration.java
+++ src/com/andrewyunt/warfare/configuration/ServerConfiguration.java
@@ -1,6 +1,12 @@
 package com.andrewyunt.warfare.configuration;
 
-import java.io.*;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 
 import org.bukkit.configuration.file.FileConfiguration;
